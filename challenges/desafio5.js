5c51e995d01f98ce5bc0f4e3f5cb3593f2bfc177
Wagner B <wagner_mbarbosa@hotmail.com>
2022-10-17 18:48:45 -0300

    :recycle:FORMATTING: para melhor leitura

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,1 +1,4 @@
-db.produtos.find({ $or: [{ vendidos: 85 }, { curtidas: 36 }] }, { nome: 1, vendidos: 1, curtidas: 1, _id: 0 });
+db.produtos.find(
+  { $or: [{ vendidos: 85 }, { curtidas: 36 }] },
+  { nome: 1, vendidos: 1, curtidas: 1, _id: 0 },
+);
